b1e1710f7e57190db34574058b758b9a6e6a5232
Jake Gordon <jakeg@MacBook-Air-2.lan>
2023-12-08 16:32:26 -0500

    switch to text, reformat

--- phirefly/src/app/File.tsx
+++ phirefly/src/app/File.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import styles from "./page.module.css";
 
 const File = ({ fileObject }: any) => {
-  const [showPreview, setShowPreview] = useState('');
+  // show preview is an array of strings
+  const [showPreview, setShowPreview] = useState<string[]>([]);;
   // const [previewContent, setPreviewContent] = useState('');
+
+  const [isSummaryShown, setIsSummaryShown] = useState(false); // State to track if summary is shown
+
+
   const file = fileObject;
 
   const renderFilePreview = () => {
@@ -52,50 +58,53 @@ const File = ({ fileObject }: any) => {
       method: 'POST',
       body: formData,
     })
-    .then(response => response.json())
+    .then(response => response.text())
     .then(data => {
       console.log(data);
       // convert json value into string
-      const dataString = JSON.stringify(data);
+      const paragraphs = data.split('\n');
+      // let dataString = data;
+      // let dataString = JSON.stringify(data);
       // const dataString = JSON.stringify(data['extracted_text']);
       // setShowPreview(data);
-      setShowPreview(dataString); 
-      // Handle the preview data
-      // display data to the screen
-      // For example, setting it to state and then displaying it
+
+      // format the data
+      // dataString.replace(/\\n/g, '\n');
+
+      setShowPreview(paragraphs); 
+      setIsSummaryShown(true); // Update the state to indicate the summary has been shown
     })
     .catch(error => {
-      // Handle any errors here
+      console.error('Error fetching the summary:', error);
     });
   }; 
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        cursor: "move",
-        width: 800,
-        height: 50,
-        backgroundColor: "grey",
-      }}
-    >
-      <button
-        onClick={handleShowPreview}
-        style={{ marginRight: 24, marginLeft: 4, width: 150 }}
-      >
-        Show Preview
-      </button>
-      <div className="preview-container">
-        {showPreview}
-      </div>
-      <p style={{ width: 400 }}> {file?.name}</p>
-
-      <p style={{ marginLeft: 12, width: 200 }}>
-        {formatUnixTime(file?.lastModified)}
-      </p>
-
-      {showPreview && preview}
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: 800 }}>
+      {!isSummaryShown && (
+        <>
+          <div style={{ display: "flex", alignItems: "center", cursor: "move", height: 50, backgroundColor: "grey" }}>
+            <button
+              onClick={handleShowPreview}
+              style={{ marginRight: 24, marginLeft: 4, width: 150 }}
+            >
+              Summarize Document
+            </button>
+            <p style={{ width: 400 }}> {file?.name}</p>
+            <p style={{ marginLeft: 12, width: 200 }}>
+              {formatUnixTime(file?.lastModified)}
+            </p>
+          </div>
+        </>
+      )}
+      {/* This will now only show after the summary is fetched */}
+      {isSummaryShown && (
+  <div className={styles.summaryContainer} style={{ width: "100%", backgroundColor: "lightgrey" }}>
+    {showPreview.map((paragraph, index) => (
+      <p key={index} className={styles.previewParagraph}>{paragraph}</p> // Render each string in a <p> tag
+    ))}
+  </div>
+)}
     </div>
   );
 };
--- phirefly/src/app/page.tsx
+++ phirefly/src/app/page.tsx
@@ -11,19 +11,47 @@ function FileUploadComponent() {
   const { fileData, addNewFiles } = useContext(FileContext);
   const [files, setFiles] = useState<File[]>([]);
 
+  // track if file is picked
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+
   const sendFilesToServer = (files: any) => {
     // add stuff here
+    const formData = new FormData();
+    formData.append("image", files[0]);
+    // fetch("http://localhost:5000/ocr", {
+    //   // add cors
+    //   mode: "cors",
+    //   method: "POST",
+    //   body: formData,
+    // })
+  };
+  // add handleButtonClick function
+  const handleButtonClick = () => {
+    const fileInput = document.getElementById("hiddenFileInput");
+    fileInput?.click();
   };
 
   const handleUpload = (event: any) => {
     const uploadedFiles: File[] = Array.from(event.target.files);
+    if (uploadedFiles.length > 0) {
+      setIsFilePicked(true); // Update the state to true when files are picked
+    };
     addNewFiles(uploadedFiles);
     sendFilesToServer(uploadedFiles);
   };
 
   return (
     <div>
-      <input type="file" multiple onChange={handleUpload} />
+      {!isFilePicked && (
+      <button className={styles.fileUploadBtn} onClick={handleButtonClick}>Please upload your health document</button>
+      )}
+      <input type="file"
+       id="hiddenFileInput"
+       multiple 
+       onChange={handleUpload} 
+       style={{ display: 'none' }} 
+       /> 
       <ul>
         {files.map((file, index) => (
           <li key={index}>{file?.name}</li>
@@ -53,31 +81,6 @@ export default function Home() {
         <FileSystem />
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
         </div>
       </FileContextProvider>
     </main>
